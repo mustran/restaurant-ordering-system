955658a2beddf3a79c30850d356f2fc41250e047
mustran <musli.ademi@inplayer.com>
2020-04-09 23:50:01 +0200

    Add service Impl files

---
+++ src/main/java/com/example/restaurantordering/services/impl/IngredientServiceImpl.java
@@ -0,0 +1,9 @@
+package com.example.restaurantordering.services.impl;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class IngredientServiceImpl {
+}
